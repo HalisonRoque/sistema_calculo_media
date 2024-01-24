e23267d262ae9c3f00b8005bf67775c231c9470e
HalisonRoque <halisonyasha@gmail.com>
2024-01-24 20:24:03 -0300

    nova classe static

---
+++ primeiro-projeto-java/src/cursojava/constantes/StatusAluno.java
@@ -0,0 +1,11 @@
+package cursojava.constantes;
+
+public class StatusAluno {
+	/* classe java para uso em todo o sistema - CLASSE E CONSTANTES ESTATICAS
+	 * se ac essa o nome da classe.atributo diretamente*/
+	public static String APROVADO = "Aprovado";
+	public static String RECUPERACAO = "Recuperacao";
+	public static String REPROVADO = "Reprovado";
+	
+
+}
